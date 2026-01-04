18bb2de4d4113c378fd0cbf9f5f5dbaf27d79056
Vahide Babaiyan <93400640+vahide-b-84@users.noreply.github.com>
2026-01-04 10:54:12 +0330

    Refactor DQN parameter handling in params.py
    Reorganized DQN-related parameters into Global_DQN (level-1) and Local_DQN (level-2) dictionaries with updated keys to match new parameter names. Removed unused imports and cleaned up class attributes for clarity and maintainability.

--- params.py
+++ params.py
@@ -1,27 +1,22 @@
 # params.py
 from configuration import parameters
 from env_extractors import extract_max_speed_from_rou, estimate_max_e2e_delay
-import pandas as pd
 class params:
-    
-    
-   
-    model_summary = parameters.model_summary  # Options: "dqn", "original_only", "greedy"
+    model_summary = parameters.model_summary  ## Option: "dqn"
+    missing_data_p = parameters.missing_data_p 
+    trajectory_noise_p = parameters.trajectory_noise_p 
+    scenario=parameters.scenario
+
     min_computation_demand = None
     SCENARIO_TYPE = parameters.SCENARIO_TYPE
     Permutation_Number = parameters.Permutation_Number
-    
-    additional_episodes=parameters.additional_episodes
-
     Alpha = parameters.compute_Alpha()
     alpha_edge = (None, None)
     alpha_cloud = (None, None)
-
     NUM_EDGE_SERVERS = parameters.NUM_EDGE_SERVERS
     NUM_CLOUD_SERVERS = parameters.NUM_CLOUD_SERVERS
     RSUs_EDGE_SERVERS = parameters.RSUs_EDGE_SERVERS
     RSU_radius = parameters.RSU_radius
-
     num_vehicles = parameters.num_vehicles
     task_result_size = parameters.task_result_size
     TASK_ARRIVAL_RATE_range = parameters.TASK_ARRIVAL_RATE_range
@@ -32,12 +27,8 @@ class params:
     Low_demand, High_demand = parameters.Low_demand, parameters.High_demand 
     EDGE_PROCESSING_FREQ_RANGE= parameters.EDGE_PROCESSING_FREQ_RANGE
     CLOUD_PROCESSING_FREQ_RANGE=parameters.CLOUD_PROCESSING_FREQ_RANGE
-
     MAX_VEHICLE_SPEED = extract_max_speed_from_rou()
     Max_e2e_Delay = estimate_max_e2e_delay()
-
-
-
     #network
     rsu_to_edge_profile = parameters.rsu_to_edge_profile
     rsu_to_cloud_profile = parameters.rsu_to_cloud_profile
@@ -48,38 +39,34 @@ class params:
     Q_alpha = parameters.Queuing_alpha
     beta = parameters.beta
 
-
-
     # Global DQN
-    Global = {
-        'activation': parameters.global_af,
-        'hidden_layers': parameters.global_hidden_layers,
-        'lr': parameters.global_lr,
-        'gamma': parameters.global_gamma,
-        'tau': parameters.global_tau,
-        'buffer_capacity': parameters.global_buffer_capacity,
-        'batch_size': parameters.global_batch_size,
-        'epsilon_start': parameters.global_epsilon_start,
-        'epsilon_end': parameters.global_epsilon_end,
-        'epsilon_decay': parameters.global_epsilon_decay,
-        'warmup_episodes': parameters.global_warmup_episodes,
-        'load_penalty_weight' : parameters.global_load_penalty_weight,
-        'epsilon':parameters.global_epsilon
+    Global_DQN = {
+        'activation': parameters.global_af_dqn,
+        'hidden_layers': parameters.global_hidden_layers_dqn,
+        'lr': parameters.global_lr_dqn,
+        'gamma': parameters.global_gamma_dqn,
+        'tau': parameters.global_tau_dqn,
+        'buffer_capacity': parameters.global_buffer_capacity_dqn,
+        'batch_size': parameters.global_batch_size_dqn,
+        'epsilon_start': parameters.global_epsilon_start_dqn,
+        'epsilon_end': parameters.global_epsilon_end_dqn,
+        'epsilon_decay': parameters.global_epsilon_decay_dqn,
     }
 
     # Local DQN
-    Local = {
-        'activation': parameters.local_af,
-        'hidden_layers': parameters.local_hidden_layers,
-        'lr': parameters.local_lr,
-        'gamma': parameters.local_gamma,
-        'tau': parameters.local_tau,
-        'buffer_capacity': parameters.local_buffer_capacity,
-        'batch_size': parameters.local_batch_size,
-        'epsilon_start': parameters.local_epsilon_start,
-        'epsilon_end': parameters.local_epsilon_end,
-        'epsilon_decay': parameters.local_epsilon_decay,
-        'warmup_episodes': parameters.local_warmup_episodes,
-        'epsilon': parameters.local_epsilon
+    Local_DQN = {
+        'activation': parameters.local_af_dqn,
+        'hidden_layers': parameters.local_hidden_layers_dqn,
+        'lr': parameters.local_lr_dqn,
+        'gamma': parameters.local_gamma_dqn,
+        'tau': parameters.local_tau_dqn,
+        'buffer_capacity': parameters.local_buffer_capacity_dqn,
+        'batch_size': parameters.local_batch_size_dqn,
+        'epsilon_start': parameters.local_epsilon_start_dqn,
+        'epsilon_end': parameters.local_epsilon_end_dqn,
+        'epsilon_decay': parameters.local_epsilon_decay_dqn,
     }
 
+
+    Global = Global_DQN
+    Local = Local_DQN
